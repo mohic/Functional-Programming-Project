03464a9f611fa1c3a67a5eb67b4302a638e585a5
gta126 <gta126705@hotmail.com>
2013-11-21 11:39:16 +0100

    Ajouts des cours

--- Projet_FP/src/main.scala
+++ Projet_FP/src/main.scala
@@ -3,34 +3,27 @@ import JaCoP.scala._
 object main extends jacop {
   def main(args: Array[String]): Unit = {
     // association valeurs => noms
-    val jourNoms = "Lundi" :: "Mardi" :: "Mercredi" :: "Jeudi" :: "Vendredi" :: Nil
+    val jourNoms  = "Lundi" :: "Mardi" :: "Mercredi" :: "Jeudi" :: "Vendredi" :: Nil
     val heureNoms = "8h30" :: "9h30" :: "10h30" :: "11h30" :: "13h00" :: "14h00" :: "15h00" :: "16h00" :: Nil
     val localNoms = "017" :: "019" :: Nil
-    val profNoms = "Donatien" :: "Brigitte" :: Nil
+    
+    val profNoms  = "Donatien" :: "Brigitte" :: Nil
+    val coursNoms = "Algorithme" :: "Calcul numérique" :: "Scala" :: Nil
 
     // variables
-    val jour = IntVar("jour", 1, 5)
-    val heure = IntVar("heure", 1, 8)
-    val local = IntVar("local", 1, 2)
+    val jour = IntVar("jour", 1, jourNoms.length)
+    val heure = IntVar("heure", 1, heureNoms.length)
+    val local = IntVar("local", 1, localNoms.length)
     val jhl = jour * 100 + heure * 10 + local
-    val prof = IntVar("prof", 1, 2)
+    
+    val prof = IntVar("prof", 1, profNoms.length)
+    val cours = IntVar("cours", 1, coursNoms.length)
 
     // tableau de variables
-    val vars = List(jhl, prof)
+    val vars = List(jhl, prof, cours)
 
-    /* donatien donne pas cours le mercredi
-	
-	
-	if (prof == donatien)
-	  jour != mercredi
-	  
-	  donatien == prof;
-	  jour != mercredi;
-	  prof == donatien -> jour != mercredi
-	*/
     // conditions
-    //TODO
-    (prof == 1) -> (jour != 3)
+    (prof == 1) -> (jour != 3) // donatien ne donne pas cours le mercredi
 
     // affichage et satisfaction
     def printSol(): Unit = {
@@ -45,7 +38,12 @@ object main extends jacop {
           print("local = " + localNoms((v.value() % 10) - 1) + " ")
         } else {
           print(v.id + " = ")
-          print(profNoms(v.value() - 1))
+          
+          if (v.id == prof.id) {
+        	  print(profNoms(v.value() - 1))
+          } else if (v.id == cours.id) {
+        	  print(coursNoms(v.value() - 1))
+          }
         }
 
         print(" ")
