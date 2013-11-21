0270e836aeb1d7f70385b4e4dcde1b10b032774c
gta126 <gta126705@hotmail.com>
2013-11-21 11:04:29 +0100

    Main de base fonctionel avec une condition: Donatien donne pas cours le mercredi

--- Projet_FP/src/main.scala
+++ Projet_FP/src/main.scala
@@ -3,27 +3,23 @@ import JaCoP.scala._
 object main extends jacop {
   def main(args: Array[String]): Unit = {
     // association valeurs => noms
-    val jourNoms  = "Lundi" :: "Mardi" :: "Mercredi" :: "Jeudi" :: "Vendredi" :: Nil
+    val jourNoms = "Lundi" :: "Mardi" :: "Mercredi" :: "Jeudi" :: "Vendredi" :: Nil
     val heureNoms = "8h30" :: "9h30" :: "10h30" :: "11h30" :: "13h00" :: "14h00" :: "15h00" :: "16h00" :: Nil
     val localNoms = "017" :: "019" :: Nil
-    val profNoms  = "Donatien" :: "Brigitte" :: Nil
+    val profNoms = "Donatien" :: "Brigitte" :: Nil
+
+    // variables
+    val jour = IntVar("jour", 1, 5)
+    val heure = IntVar("heure", 1, 8)
+    val local = IntVar("local", 1, 2)
+    val jhl = jour * 100 + heure * 10 + local
+    val prof = IntVar("prof", 1, 2)
+
+    // tableau de variables
+    val vars = List(jhl, prof)
+
+    /* donatien donne pas cours le mercredi
 	
-	// variables
-    val jours  = for(i <- List.range(0, jourNoms.length)) yield IntVar(jourNoms(i), 1, jourNoms.length)
-    val heures = for(i <- List.range(0, heureNoms.length)) yield IntVar(heureNoms(i), 1, heureNoms.length)
-    val locaux = for(i <- List.range(0, localNoms.length)) yield IntVar(localNoms(i), 1, localNoms.length)
-    val profs  = for(i <- List.range(0, profNoms.length)) yield IntVar(profNoms(i), 1, profNoms.length)
-    
-    // conditions
-    alldifferent(jours)
-    alldifferent(heures)
-    alldifferent(locaux)
-    alldifferent(profs)
-	
-	// tableau de variables
-    val vars = jours ::: heures ::: locaux ::: profs
-	
-	/* donatien donne pas cours le mercredi
 	
 	if (prof == donatien)
 	  jour != mercredi
@@ -32,18 +28,32 @@ object main extends jacop {
 	  jour != mercredi;
 	  prof == donatien -> jour != mercredi
 	*/
-	// conditions
-	//TODO
-	
-	// affichage et satisfaction
-	def printSol(): Unit = {
-	  for (v <- vars) {
-	    print(v.id + " " + v.value() + " ")
-	  }
-	  
-	  println()
-	}
-	
-	val result = satisfyAll(search(vars, first_fail, indomain_middle), printSol)
+    // conditions
+    //TODO
+    (prof == 1) -> (jour != 3)
+
+    // affichage et satisfaction
+    def printSol(): Unit = {
+      for (v <- vars) {
+        if (v.id == jhl.id) {
+          var i = v.value() / 100
+          print("jour = " + jourNoms(i - 1) + " ")
+
+          i = v.value() - (i * 100)
+          print("heure = " + heureNoms((i / 10) - 1) + " ")
+
+          print("local = " + localNoms((v.value() % 10) - 1) + " ")
+        } else {
+          print(v.id + " = ")
+          print(profNoms(v.value() - 1))
+        }
+
+        print(" ")
+
+        println()
+      }
+    }
+
+    val result = satisfyAll(search(vars, first_fail, indomain_middle), printSol)
   }
 }
