6cdf4af1d42cc55391d1d0a4d1006ed10821cbf5
gta126 <gta126705@hotmail.com>
2013-12-12 14:11:34 +0100

    ok

--- Projet_FP/src/Test.scala
+++ Projet_FP/src/Test.scala
@@ -16,14 +16,18 @@ object Test extends jacop {
     	yield jours(j) + '_' + heures(h) + '_' + locaux(l)
     
     // liste des profs
-    val prof  = List("AUCUN", "Donatien", "Brigitte", "Gilles")
+    val prof  = List("AUCUN", "Donatien", "Thierry", "Gilles", "Sonia")
     
     // liste des cours
-    val cours = List("AUCUN", "Algorithme", "Scala", "IOO", "BI", "JSP")
+    val cours = List("AUCUN", "Anglais", "Scala", "C++", "BI", "SAP")
+    
+    // liste des séries
+    val series = List("AUCUN", "A", "B")
     
     // listes de IntVar pour indiquer des contraintes
     val jhl_prof  = for (i <- List.range(0, jhl.length)) yield IntVar("jhl_prof_" + i, 1, prof.length)
     val jhl_cours = for (i <- List.range(0, jhl.length)) yield IntVar("jhl_cours_" + i, 1, cours.length)
+    val jhl_serie = for (i <- List.range(0, jhl.length)) yield IntVar("jhl_serie_" + i, 1, series.length)
     
     // ******** functions
     
@@ -86,6 +90,15 @@ object Test extends jacop {
     }
     
     /**
+     * Obtenir l'index d'une série
+     * @params nomCours Le nom de la série
+     * @return L'index d'une série à partir de 1. 0 si pas trouvé
+     * */
+    def getSerie(nomSerie: String): Int = {
+      series.indexOf(nomSerie) + 1
+    }
+    
+    /**
      * Définit une contrainte disant que le prof ne donne pas cours tel jour de la semaine
      * @params prof Le nom du professeur
      * @params jour Le jour de la semaine
@@ -116,12 +129,12 @@ object Test extends jacop {
     }
     
     /**
-     * Définit une contrainte disant que le professeur donne un certain cours durant maximum autant d'heures
+     * Définit une contrainte disant que le professeur donne un certain cours durant autant d'heures
      * @params prof Le nom du professeur
      * @params cours Le nom du cours
-     * @params heuresMax Le nombre d'heures maximum
+     * @params nbrHeures Le nombre d'heures
      * */
-    def profDonneCours(prof: String, cours: String, heuresMax: Int): Unit = {
+    def profDonneCours(prof: String, cours: String, nbrHeures: Int): Unit = {
       var lst = List[BoolVar]()
       
       for (i <- List.range(0, jhl.length)) {
@@ -131,15 +144,33 @@ object Test extends jacop {
         lst ::= state
       }
       
-      sum(lst) #= heuresMax
+      sum(lst) #= nbrHeures
     }
     
+    /**
+     * Définit une contrainte disant que la série à un certain cours durant autant d'heures
+     * @params serie Le nom de la série
+     * @params cours Le nom du cours
+     * @params nbrHeures Le nombre d'heures
+     * */
+    def serieCours(serie: String, cours: String, nbrHeures: Int): Unit = {
+      var lst = List[BoolVar]()
+      
+      for (i <- List.range(0, jhl.length)) {
+        val state = BoolVar("cours-serie")
+        
+        state <=> AND(jhl_serie(i) #= getSerie(serie), jhl_cours(i) #= getCours(cours))
+        lst ::= state
+      }
+      
+      sum(lst) #= nbrHeures
+    }
     
     // contraintes obligatoires
     
-    // interdire d'avoir cours si pas de prof et inversément
+    // interdire d'avoir cours si pas de prof et inversément et idem avec les séries
     for (i <- List.range(0, jhl.length))
-      OR(AND(jhl_prof(i) #= 1, jhl_cours(i) #= 1), AND(jhl_prof(i) #\= 1, jhl_cours(i) #\= 1))
+      OR(AND(jhl_prof(i) #= 1, jhl_cours(i) #= 1, jhl_serie(i) #= 1), AND(jhl_prof(i) #\= 1, jhl_cours(i) #\= 1, jhl_serie(i) #\= 1))
       
     // un prof ne peut pas être dans plusieurs local en même temps
     for (i <- List.range(0, jhl.length, locaux.length)) {
@@ -150,50 +181,99 @@ object Test extends jacop {
     
     // ******************
     
+    // jour d'absence des profs
+    profDonnePasCoursJour("Donatien", "Mercredi")
+    
+    profDonnePasCoursJour("Brigitte", "Jeudi")
+    
+    profDonnePasCoursJour("Gilles", "Lundi")
+    profDonnePasCoursJour("Gilles", "Mercredi")
+    profDonnePasCoursJour("Gilles", "Jeudi")
+    profDonnePasCoursJour("Gilles", "Vendredi")
+    
+    profDonnePasCoursJour("Emmeline", "Vendredi")
+    
+    // heure d'arrivée des profs
+    profDonnePasCoursAvantHeure("Donatien", "10h30")
+    profDonnePasCoursAvantHeure("Brigitte", "13h00")
+    profDonnePasCoursAvantHeure("Gilles", "9h30")
+    
+    // série a cours de ... pendant ... heures
+    serieCours("A", "Anglais", 2)
+    serieCours("B", "Anglais", 2)
+    
+    serieCours("A", "BI", 2)
+    serieCours("B", "BI", 2)
+    
+    serieCours("A", "C++", 2)
+    serieCours("A", "Scala", 2)
+    
+    serieCours("B", "SAP", 2)
+    serieCours("B", "Scala", 2)
+    
+    // prof donne cours pendant ... heures
+    profDonneCours("Sonia", "Anglais", 4)
+    profDonneCours("Gilles", "BI", 4)
+    profDonneCours("Thierry", "C++", 2)
+    profDonneCours("Donatien", "Scala", 4)
+    profDonneCours("Thierry", "SAP", 2)
+    
+    
+    //TODO: profDonnePasCoursApres... profDonnePasCoursATelHeure...
+    
+    
+    
 //    profDonnePasCoursJour("Donatien", "Vendredi")
 //    profDonnePasCoursJour("Brigitte", "Lundi")
 //    profDonnePasCoursJour("Brigitte", "Mardi")
 //    profDonnePasCoursJour("Brigitte", "Mercredi")
 //    profDonnePasCoursJour("Brigitte", "Jeudi")
-      profDonnePasCoursAvantHeure("Donatien", "9h30")
-    profDonneCours("Donatien", "IOO", 4)
-//    profDonneCours("Donatien", "Scala", 5) //TODO: QUESTION: Si appeller 2x cette fonction avec le même prof, tourne en boucle. Pour régler, soit ne pas dire 2x le même prof, soit retirer des cours et/ou des profs dans les listes
-//    profDonneCours("Brigitte", "Scala", 4)
+    //profDonnePasCoursAvantHeure("Donatien", "9h30")
+//    profDonneCours("Donatien", "IOO", 4)
+    //profDonneCours("Donatien", "Scala", 4)
+//    serieACours("A", "IOO", 3)
+//    serieACours("B", "IOO", 3)
+//    profDonneCours("Brigitte", "IOO", 2)
 //    profDonneCours("Gilles", "BI", 4)
     
-    val vars = jhl_prof ::: jhl_cours
+    val vars = jhl_prof ::: jhl_cours ::: jhl_serie
     
     // compteur de solutions
     //var compteur = 0;
     
     def printSol(): Unit = {
-      var result = Map[String, (String, String)]()
+      var result = Map[String, (String, String, String)]()
       
       // création des différentes entrées
       for (v <- jhl) {
-        result += (v -> ("", ""))
+        result += (v -> ("", "", ""))
       }
       
       // remplissage des entrées
       for(v <- vars) {
-        val id = jhl(v.id.replaceAll("jhl_prof_", "").replaceAll("jhl_cours_", "").toInt)
+        val id = jhl(v.id.replaceAll("jhl_prof_", "").replaceAll("jhl_cours_", "").replaceAll("jhl_serie_", "").toInt)
         
         if (v.id.startsWith("jhl_prof_")) {
           if (v.value() - 1 == 0)
-            result += (id -> ("°", result(id)._2))
+            result += (id -> ("°", result(id)._2, result(id)._3))
           else
-        	  result += (id -> (prof(v.value() - 1), result(id)._2))
+        	  result += (id -> (prof(v.value() - 1), result(id)._2, result(id)._3))
         } else if (v.id.startsWith("jhl_cours_")) {
           if (v.value() - 1 == 0)
-            result += (id -> (result(id)._1, "°"))
+            result += (id -> (result(id)._1, "°", result(id)._3))
           else
-        	  result += (id -> (result(id)._1, cours(v.value() - 1)))
+        	  result += (id -> (result(id)._1, cours(v.value() - 1), result(id)._3))
+        } else if (v.id.startsWith("jhl_serie_")) {
+          if (v.value() - 1 == 0)
+            result += (id -> (result(id)._1, result(id)._2, "°"))
+          else
+        	  result += (id -> (result(id)._1, result(id)._2, series(v.value() - 1)))
         }
       }
       
       // affichage des entrées dans l'ordre du lundi 8h30 au 017 jusqu'au vendredi 16h00 au 019
       for (v <- jhl) {
-        println(v + " - " + result.get(v).get._1 + " - " + result.get(v).get._2)
+        println(v + " - " + result.get(v).get._1 + " - " + result.get(v).get._2 + " - " + result.get(v).get._3)
       }
       
       //compteur += 1
